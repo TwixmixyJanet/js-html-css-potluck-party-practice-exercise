5e0ff54341eba020188586f68319232507048b6d
Janet Webster <twixmixy@gmail.com>
2023-02-16 11:40:25 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Change Instagram icon color
+var instagram = document.querySelector(".fa-instagram");
+//console.log(instagram);
+instagram.style.color = "tomato";
+
+// Add background image
+var foodImage = document.querySelector(".food");
+//console.log(foodImage);
+foodImage.style.backgroundImage = "url('../img/victoria-shes-unsplash.jpg')";
+
+// Dotted border-bottom around h1
+var noms = document.querySelector("h1");
+noms.style.borderBottom = "4px dotted tomato";
+
+var kindabig = document.querySelector(".kindabig");
+//console.log(kindabig);
+kindabig.style.lineHeight = "1em";
